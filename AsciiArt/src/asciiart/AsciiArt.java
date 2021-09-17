49127f0b13f148412e86de3f33b56db1e440eb5f
ldavoll <20544@my4county.net>
2021-09-17 09:33:19 -0400

    AsciiArt assignment done.

---
+++ AsciiArt/src/asciiart/AsciiArt.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package asciiart;
+
+/**
+ *
+ * @author ldavoll
+ */
+public class AsciiArt {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+ System.out.println("---W.D.GASTER---");
+        System.out.println("     OOOOOO     ");
+        System.out.println("    O      OO   ");
+        System.out.println("   OO        O  ");
+        System.out.println("  O O         O ");
+        System.out.println(" O  O     OO  O ");
+        System.out.println("O   O    OOOO O ");
+        System.out.println("O  O   O OOOOO O");
+        System.out.println("O  OO  O OOOOO O");
+        System.out.println("O   O OO OOOOO O");
+        System.out.println("O O OOOO OOOOO O");
+        System.out.println("O OOOOOO  OOO  O");
+        System.out.println("O  OOOO   O    O");
+        System.out.println("O   OO    O    O");
+        System.out.println(" O        O    O");
+        System.out.println(" O O      OO   O");
+        System.out.println(" O OO     OO  O ");
+        System.out.println("  O OO  OOO   O ");
+        System.out.println("  O  OOOOO   O  ");
+        System.out.println("   O  OOOO   O  ");
+        System.out.println("   O   OO   O   ");
+        System.out.println("    O      O    ");
+        System.out.println("     OOOOOO     ");
+        System.out.println("---W.D.GASTER---");
+
+
+    }
+    
+}
---
+++ HelloWorld/src/helloworld/HelloWorld.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package helloworld;
+
+/**
+ *
+ * @author ldavoll
+ */
+public class HelloWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World! I am attempting to write a message to"
+                + "you from a world beyond your understanding. A world that puny humans can't comprehend.");
+    }
+    
+}
